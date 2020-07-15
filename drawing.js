5957cbca67201d387f11487f57c33c5839742df2
Davide <savo96@outlook.com>
2020-07-15 03:28:14 +0200

    edits

--- drawing.js
+++ drawing.js
@@ -3,6 +3,12 @@ var baseDir;
 var shaderDir;
 var program;
 
+objectsInScene = new Array();
+
+objectsInScene[0] = [[-4.0, 0.0, 0.0], 1.5]
+objectsInScene[1] = [[0.0, 0.0, 0.0], 1.5]
+objectsInScene[2] = [[4.0, 0.0, 0.0], 1.5]
+
 //Parameters for Camera
 var cx = -40.0;
 var cy = 25.0;
@@ -506,3 +512,96 @@ function moveCameraBackward(){
 
 //---------------------------------------------------------------------------------------------------------
 
+//-----------------------------------Raycast---------------------------------------------------------------
+
+
+function raySphereIntersection(rayStartPoint, rayNormalisedDir, sphereCentre, sphereRadius){
+  //Distance between sphere origin and origin of ray
+  var l = [sphereCentre[0] - rayStartPoint[0], sphereCentre[1] - rayStartPoint[1], sphereCentre[2] - rayStartPoint[2]];
+  var l_squared = l[0] * l[0] + l[1] * l[1] + l[2] * l[2];
+  //If this is true, the ray origin is inside the sphere so it collides with the sphere
+  if(l_squared < (sphereRadius*sphereRadius)){
+      console.log("ray origin inside sphere");
+      return true;
+  }
+  //Projection of l onto the ray direction 
+  var s = l[0] * rayNormalisedDir[0] + l[1] * rayNormalisedDir[1] + l[2] * rayNormalisedDir[2];
+  //The spere is behind the ray origin so no intersection
+  if(s < 0){
+      console.log("sphere behind ray origin");
+      return false;
+  }
+  //Squared distance from sphere centre and projection s with Pythagorean theorem
+  var m_squared = l_squared - (s*s);
+  //If this is true the ray will miss the sphere
+  if(m_squared > (sphereRadius*sphereRadius)){
+      console.log("m squared > r squared");
+      return false;
+  }
+  //Now we can say that the ray will hit the sphere 
+  console.log("hit");
+  return true;
+  
+}
+
+function myOnMouseUp(ev){
+  //These commented lines of code only work if the canvas is full screen
+  /*console.log("ClientX "+ev.clientX+" ClientY "+ev.clientY);
+  var normX = (2*ev.clientX)/ gl.canvas.width - 1;
+  var normY = 1 - (2*ev.clientY) / gl.canvas.height;
+  console.log("NormX "+normX+" NormY "+normY);*/
+
+  //This is a way of calculating the coordinates of the click in the canvas taking into account its possible displacement in the page
+  var top = 0.0, left = 0.0;
+  canvas = gl.canvas;
+  while (canvas && canvas.tagName !== 'BODY') {
+      top += canvas.offsetTop;
+      left += canvas.offsetLeft;
+      canvas = canvas.offsetParent;
+  }
+  console.log("left "+left+" top "+top);
+  var x = ev.clientX - left;
+  var y = ev.clientY - top;
+      
+  //Here we calculate the normalised device coordinates from the pixel coordinates of the canvas
+  console.log("ClientX "+x+" ClientY "+y);
+  var normX = (2*x)/ gl.canvas.width - 1;
+  var normY = 1 - (2*y) / gl.canvas.height;
+  console.log("NormX "+normX+" NormY "+normY);
+
+  //We need to go through the transformation pipeline in the inverse order so we invert the matrices
+  var projInv = utils.invertMatrix(perspectiveMatrix);
+  var viewInv = utils.invertMatrix(viewMatrix);
+  
+  //Find the point (un)projected on the near plane, from clip space coords to eye coords
+  //z = -1 makes it so the point is on the near plane
+  //w = 1 is for the homogeneous coordinates in clip space
+  var pointEyeCoords = utils.multiplyMatrixVector(projInv, [normX, normY, -1, 1]);
+  console.log("Point eye coords "+pointEyeCoords);
+
+  //This finds the direction of the ray in eye space
+  //Formally, to calculate the direction you would do dir = point - eyePos but since we are in eye space eyePos = [0,0,0] 
+  //w = 0 is because this is not a point anymore but is considered as a direction
+  var rayEyeCoords = [pointEyeCoords[0], pointEyeCoords[1], pointEyeCoords[2], 0];
+
+  
+  //We find the direction expressed in world coordinates by multipling with the inverse of the view matrix
+  var rayDir = utils.multiplyMatrixVector(viewInv, rayEyeCoords);
+  console.log("Ray direction "+rayDir);
+  var normalisedRayDir = normaliseVector(rayDir);
+  console.log("normalised ray dir "+normalisedRayDir);
+  //The ray starts from the camera in world coordinates
+  var rayStartPoint = [cx, cy, cz];
+  
+  //We iterate on all the objects in the scene to check for collisions
+  for(i = 0; i < objectsInScene.length; i++){
+      var hit = raySphereIntersection(rayStartPoint, normalisedRayDir, objectsInScene[i][0], objectsInScene[i][1]);
+      if(hit){
+          console.log("hit sphere number "+i);
+          colours[i] = [Math.random(), Math.random(), Math.random(), 1];
+      }
+  }
+}
+
+
+window.addEventListener("mouseup", myOnMouseUp);
