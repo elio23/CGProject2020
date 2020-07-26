c8fa5de324b7c12ce9287e78479a0a031bc0b8b5
Elio <elio.salvini@mail.polimi.it>
2020-07-26 14:56:02 +0200

    getLocalMatrixFixed again
    Also scaling shold be applaied at first (according to professour slides)

--- drawing.js
+++ drawing.js
@@ -238,10 +238,10 @@ window.onload = init();
 function getLocalMatrix(position, rotation, scale) {
     let matricesList = [
         utils.MakeTranslateMatrix(position[0], position[1], position[2]),
-        utils.MakeScaleMatrix(scale[0], scale[1], scale[2]),
         utils.MakeRotateXMatrix(rotation[0]),
         utils.MakeRotateYMatrix(rotation[1]),
         utils.MakeRotateZMatrix(rotation[2]),
+        utils.MakeScaleMatrix(scale[0], scale[1], scale[2]),
     ];
     return utils.multiplyListOfMatrices(matricesList);
 }
